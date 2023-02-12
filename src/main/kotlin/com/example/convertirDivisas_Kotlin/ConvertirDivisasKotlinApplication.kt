a91a54aa00fa95f05b0b3bc9abd68d17cf07973a
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-02-12 09:03:43 -0400

    Proyecto creado

---
+++ src/main/kotlin/com/example/convertirDivisas_Kotlin/ConvertirDivisasKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.convertirDivisas_Kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConvertirDivisasKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConvertirDivisasKotlinApplication>(*args)
+}
